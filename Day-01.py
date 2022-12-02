c1d53a7fa057dda1f6f22019e4006b3cfae5650e
James Pain <james@thinkbit.uk>
2022-12-02 19:25:05 +0000

    Added comments to Day 1 and 2

--- Day-01.py
+++ Day-01.py
@@ -2,6 +2,7 @@
 import time
 
 def part1():
+    # Given input of groups of numbers, find the group with the highest sum.
     print('You wanna know the elf with most beef? We\'ve got you')
     beefiestElf = 0
     howBeefIsElf = 0
@@ -10,6 +11,9 @@ def part1():
         for line in fileInput.readlines():
             line = line.strip()
             if line == '':
+                # I realised afterwards that this may not trigger after the last group
+                # if the input doesn't have a blank line at the end, so the last group
+                # may not get compared and added to the highest number total.
                 if howBeefIsElf > beefiestElf:
                     beefiestElf = howBeefIsElf
                 howBeefIsElf = 0
@@ -20,6 +24,8 @@ def part1():
         print('That\'s a lot of beef!')
 
 def part2():
+    # Given input of groups of numbers, find the top 3 groups with the highest 
+    # sums and return sum of the 3 groups.
     print('Oh, you want the top 3 combined now? How very agile of you.')
     howBeefIsElf = 0
     listOfBeef = []
@@ -33,6 +39,10 @@ def part2():
             else:
                 howBeefIsElf = howBeefIsElf + int(line)
         listOfBeef.sort(reverse=True)
+        # I don't like adding the whole input into a list and running a
+        # sort on the whole thing. It's quite wasteful of memory.
+        # However, I'm doing one of these every day. They can't all be 
+        # winners.
         result = listOfBeef[0] + listOfBeef[1] + listOfBeef[2]
         print('Result: ' + str(result))
         if result > 99999:
@@ -40,6 +50,9 @@ def part2():
 
 if __name__ == "__main__":
     print(time.process_time_ns())
+    # I wanted to compare running time with a friend's soloution.
     part1()
     print('Part 1 time: ' + str(time.process_time_ns()))
+    # It was coming out as taking 0ns to run. I might not understand
+    # the function well enough.
     part2()
--- Day-02.py
+++ Day-02.py
@@ -2,11 +2,26 @@
 import math
 
 def part1():
+    # Calculate the score of the given input's Rock, Paper Scissors matches.
+    # Where A B C is an opponant's Rock, Paper Scissors choice, and X Y Z is your choice;
+    # Based on a scoring system of Win = 6, Tie = 3, Lose = 0 plus a bonus of what you
+    # chose as Rock = 1, Paper = 2, and Scissors = 3;
+
     throws = {
         'A': {'X': 4, 'Y': 8, 'Z': 3},
         'B': {'X': 1, 'Y': 5, 'Z': 9},
         'C': {'X': 7, 'Y': 2, 'Z': 6}
     }
+
+    # I tried to come up with a fancy mathmatical calculation to solve the result
+    # of a Rock Paper Scissor match. I tried converting the choice into a number,
+    # and running an operation on that number to determin the result. Like, if
+    # Rock = 1, and Scissors = 2, then calculate 1 + 2. If result is 3, then win.
+    # 
+    # I couldn't find a single way to calculate the result mathmatically.
+    #
+    # So I ended up hardcoding the results. I'm not happy about it, but it works.
+
     score = 0
     with open('Day-02-input.txt') as fileInput:
         for line in fileInput.readlines():
@@ -15,6 +30,7 @@ def part1():
     print(score)
 
 def part2():
+    # Calculate the score if X = lose, Y = tie, and Z = win
     throws = {
         'A': {'X': 3, 'Y': 4, 'Z': 8},
         'B': {'X': 1, 'Y': 5, 'Z': 9},
