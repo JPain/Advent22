f0a3067c6167819d736110ff85a8427bd3c9bdbe
James Pain <james@thinkbit.uk>
2022-12-06 19:35:01 +0000

    Day-05 Part 2 Done

--- Day-05.py
+++ Day-05.py
@@ -29,11 +29,32 @@ def part1():
     print(result)
 
 def part2():
+    stacks = [[] for i in range(9)]
     with open(inputFileName) as fileInput:
         for line in fileInput.readlines():
-            print('nothing')
-            # Logic here
+
+            if line[0] == '[':
+                for i in range(9):
+                    cursor = 4 * i
+                    if line[cursor] == '[':
+                        stacks[i].append(line[cursor + 1])
+            
+            if line[0] == 'm':
+                command = line.replace('move ', '').replace('from ', '').replace('to ', '').split()
+                numberToMove = int(command[0])
+                fromStackNum = int(command[1]) - 1
+                toStackNum = int(command[2]) - 1
+                extractedItems = []
+                for i in range(numberToMove):
+                    extractedItems.append(stacks[fromStackNum].pop(0))
+                for i in range(numberToMove):
+                    stacks[toStackNum].insert(0,extractedItems.pop())
+
+    result = ''
+    for stack in stacks:
+        result = result + stack[0]
+    print(result)
 
 if __name__ == "__main__":
     part1()
-    # part2()
+    part2()
