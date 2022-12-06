6d39640e56b940953aae4b8a95e814414d831025
James Pain <james@thinkbit.uk>
2022-12-06 19:23:53 +0000

    Day-05 Part 1 Done

--- Day-05.py
+++ Day-05.py
@@ -4,13 +4,34 @@ import os
 inputFileName = os.path.basename(__file__).replace('.py', '-input.txt')
 
 def part1():
+    stacks = [[] for i in range(9)]
     with open(inputFileName) as fileInput:
         for line in fileInput.readlines():
-            # Logic here
+
+            if line[0] == '[':
+                for i in range(9):
+                    cursor = 4 * i
+                    if line[cursor] == '[':
+                        stacks[i].append(line[cursor + 1])
+            
+            if line[0] == 'm':
+                command = line.replace('move ', '').replace('from ', '').replace('to ', '').split()
+                numberToMove = int(command[0])
+                fromStackNum = int(command[1]) - 1
+                toStackNum = int(command[2]) - 1
+                for i in range(numberToMove):
+                    extractedItem = stacks[fromStackNum].pop(0)
+                    stacks[toStackNum].insert(0,extractedItem)
+
+    result = ''
+    for stack in stacks:
+        result = result + stack[0]
+    print(result)
 
 def part2():
     with open(inputFileName) as fileInput:
         for line in fileInput.readlines():
+            print('nothing')
             # Logic here
 
 if __name__ == "__main__":
