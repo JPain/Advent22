aa40bcb032e50a45995a4949fcb72113c3dc1051
James Pain <james@thinkbit.uk>
2022-12-06 19:49:33 +0000

    Day 06 Part 1 Done

--- Day-06.py
+++ Day-06.py
@@ -5,12 +5,17 @@ inputFileName = os.path.basename(__file__).replace('.py', '-input.txt')
 
 def part1():
     with open(inputFileName) as fileInput:
-        for line in fileInput.readlines():
-            # Logic here
+        input = fileInput.readline()
+        for i in range(4,len(input)):
+            if len(set(input[i-4:i])) == 4:
+                print('Found {0} at {1}'.format(input[i-4:i], i))
+                return
+            
 
 def part2():
     with open(inputFileName) as fileInput:
         for line in fileInput.readlines():
+            print('nothing')
             # Logic here
 
 if __name__ == "__main__":
